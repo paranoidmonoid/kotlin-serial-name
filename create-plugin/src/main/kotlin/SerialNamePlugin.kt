6ed8d0774172a39804a72bdea153f80f923cccc4
Karin-Aleksandra Monoid <monoidaleksandra@gmail.com>
2021-07-02 15:43:24 +0200

    Initial commit

---
+++ create-plugin/src/main/kotlin/SerialNamePlugin.kt
@@ -0,0 +1,44 @@
+import arrow.meta.CliPlugin
+import arrow.meta.Meta
+import arrow.meta.invoke
+import arrow.meta.phases.CompilerContext
+import arrow.meta.phases.analysis.isAnnotatedWith
+import arrow.meta.quotes.Transform
+import arrow.meta.quotes.classDeclaration
+import org.jetbrains.kotlin.psi.psiUtil.getValueParameters
+import kotlin.contracts.ExperimentalContracts
+
+class SerialNamePlugin : Meta {
+    @ExperimentalContracts
+    override fun intercept(ctx: CompilerContext): List<CliPlugin> =
+        listOf(
+            serialNameGenerator
+        )
+}
+
+val Meta.serialNameGenerator: CliPlugin
+    get() =
+        "Serial Name Plugin" {
+            meta(
+                classDeclaration(this, { isAnnotatedWith("@Serializable".toRegex()) }) { classElement ->
+                    val skip = value.getValueParameters().all { it.isAnnotatedWith(".*SerialName.*".toRegex()) }
+                    val paramList = value.getValueParameters()
+                        .map { "@kotlinx.serialization.SerialName(\"${it.name?.toSnakeCase()}\") ${it.text}" }
+                    val paramListString = paramList.joinToString(", ", "(", ")")
+                    val newDeclaration = """
+                        |$`@annotations` $kind $name$`(typeParameters)`$paramListString {
+                        |    $body
+                        |}
+                    """.trimMargin()
+                    Transform.Companion.replace(classElement, if (skip) this else newDeclaration.`class`.syntheticScope)
+                }
+
+            )
+        }
+
+
+fun String.toSnakeCase() = splitToWords().joinToString("_").toLowerCase()
+
+// https://stackoverflow.com/questions/7593969/regex-to-split-camelcase-or-titlecase-advanced#comment57838106_7599674
+private fun String.splitToWords() =
+    split("(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|(?<=[0-9])(?=[A-Z][a-z])|(?<=[a-zA-Z])(?=[0-9])".toRegex())
---
+++ use-plugin/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+package io.arrowkt.example
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+@Serializable
+data class Cat(val name: String, val age: Int, val ownerName: String, val breed: String)
+
+@Serializable
+data class Dog(val name: String, val age: Int, val ownerName: String, val breed: String)
+
+fun main() {
+  val cat = Cat("Chonk", 3, "Max", "British Shorthair")
+  val dog = Dog("Puff", 3, "Max", "Shiba Inu")
+  println(Json.encodeToString(cat))
+  println(Json.encodeToString(dog))
+}
